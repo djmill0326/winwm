d16b61c7bcb1f1f2709aaa757fd936a45c6a9283
Dylan Miller <djmill0326@gmail.com>
2024-02-21 02:41:53 -0500

    explanation

--- vending/schema.js
+++ vending/schema.js
@@ -6,7 +6,7 @@ const schema = make_schema(["Product", "Size", "Type", "Vendor", "Cost/pkg", "Un
 const unit_price = (x) => parseFloat(x[4]) / parseFloat(x[5]);
 const margin = (x) => parseFloat(x[6]) - unit_price(x);
 const tax_rate = 0.07;
-// get mogged. random taxes
+// get mogged. random taxes (this is a bug in at least chromium, adding an external value fixes)
 const tax = (x) => unit_price(x) * tax_rate + Math.random() * 0.0001;
 schema.push({ x: "Tax", active: true, resolver: ez(tax) });
 schema.push({ x: "Unit", active: true, resolver: ez(unit_price) });
